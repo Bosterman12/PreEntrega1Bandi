13b08b4b5c69819a6c0a1200f58fff2e0c7536c9
Bosterman12 <bandi_alejandro@yahoo.com.ar>
2023-01-30 15:25:17 -0300

    arrancando el proyecto react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className='divInicial'>
+      <h1>Hola, buenas tardes</h1>
+    </div>
+  );
+}
+
+
