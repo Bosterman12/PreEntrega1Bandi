d2981c4389d7a9fb946a69e506870c5036cb6709
Bosterman12 <bandi_alejandro@yahoo.com.ar>
2023-01-30 19:46:57 -0300

    metiendo el carrito en el navbar

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {Navbar} from './Navbar/Navbar'
+import React from 'react';
+
+export const App = () => {
+  return (
+    
+    <div>
+      <Navbar nombre={"El Surubi"}/>
+    </div>
+  
+  );
+}
+
+
---
+++ src/components/Cartwidget/Cartwidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Cartwidget = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+
