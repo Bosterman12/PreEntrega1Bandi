9c4074b38ca99913dc9dd2e9718d85d0bcdb6ebc
Bosterman12 <bandi_alejandro@yahoo.com.ar>
2023-01-31 22:22:04 -0300

    metiendo el icono del carrito

--- src/components/Cartwidget/Cartwidget.jsx
+++ src/components/Cartwidget/Cartwidget.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export const Cartwidget = ({cantCarrito}) => {
     return (
         <>
-           <button className="btn btn-dark">Carrito</button>
+           <button className="btn btn-dark"><i className="bi bi-cart4"></i></button>
            <p>{cantCarrito}</p> 
         </>
     );
 }
 
-
+//<button className="btn btn-dark">Carrito</button>//
