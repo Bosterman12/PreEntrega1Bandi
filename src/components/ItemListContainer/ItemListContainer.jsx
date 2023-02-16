2d94a9f0ba0cc4dcc0cba26712f4ee4eb2acffd2
Bosterman12 <bandi_alejandro@yahoo.com.ar>
2023-02-15 22:39:29 -0300

    ahora aparece categoria en la url

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -10,7 +10,7 @@ export const ItemListContainer = () => {
    useEffect (() => {
     if(idCategoria){
         consultarBDD('../json/productos.json').then (products => {
-        const prods = products.filter(prod => prod.idCategoria === parseInt(idCategoria))
+        const prods = products.filter(prod => prod.idCategoria === (idCategoria))
         const items = ItemList ({prods})
         
         setProductos(items)
